4cbb8aac0fb32be3169f683416ab2d7c44d446aa
Alexander Bershadskyy <a.bershadskyy@ryte.com>
2017-11-11 02:37:13 +0100

    added compilation flags

--- zsh/config.zsh
+++ zsh/config.zsh
@@ -4,6 +4,8 @@ export CLICOLOR=true
 # You may need to manually set your language environment
 export LANG=en_US.UTF-8
 
+# Compilation flags
+export ARCHFLAGS="-arch x86_64"
 
 fpath=($ZSH/functions $fpath)
 
