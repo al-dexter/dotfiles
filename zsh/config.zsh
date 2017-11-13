240141c4d28940545431c4720e42a49d6230bb29
Alexander Bershadskyy <a.bershadskyy@ryte.com>
2017-11-13 17:50:39 +0100

    removed unused functions

--- zsh/config.zsh
+++ zsh/config.zsh
@@ -21,15 +21,9 @@ setopt LOCAL_TRAPS # allow functions to have local traps
 setopt HIST_VERIFY
 setopt SHARE_HISTORY # share history between sessions ???
 setopt EXTENDED_HISTORY # add timestamps to history
-setopt PROMPT_SUBST
-setopt CORRECT
 setopt COMPLETE_IN_WORD
 setopt IGNORE_EOF
 
 setopt APPEND_HISTORY # adds history
 setopt HIST_IGNORE_ALL_DUPS  # don't record dupes in history
 setopt HIST_REDUCE_BLANKS
-
-# don't expand aliases _before_ completion has finished
-#   like: git comm-[tab]
-setopt complete_aliases
