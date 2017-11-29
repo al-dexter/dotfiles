ed2d80b6c097cf4bc20fea4348d2cd3effe53075
Alexander Bershadskyy <a.bershadskyy@ryte.com>
2017-11-29 14:04:25 +0100

    add coreutils and some other packages

--- system/_path.zsh
+++ system/_path.zsh
@@ -1,2 +1,5 @@
-export PATH="/usr/local/bin:/usr/local/sbin:/usr/bin:/bin:/usr/sbin:/sbin:$DOTFILES/bin:$PATH"
-export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
+PATH="/usr/local/bin:/usr/local/sbin:/usr/bin:/bin:/usr/sbin:/sbin:$DOTFILES/bin:$PATH"
+MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
+
+export PATH="/usr/local/opt/coreutils/libexec/gnubin:$PATH"
+export MANPATH="/usr/local/opt/coreutils/libexec/gnuman:$MANPATH"
