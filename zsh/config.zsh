3fd440a2003bc6eecb590b8fbe4606023fe23a32
Alexander Bershadskyy <a.bershadskyy@ryte.com>
2018-01-04 09:48:35 +0100

    add some theme settings

--- zsh/config.zsh
+++ zsh/config.zsh
@@ -1,6 +1,7 @@
 export LSCOLORS="exfxcxdxbxegedabagacad"
 export LS_COLORS='di=34:ln=35:so=32:pi=33:ex=31:bd=34;46:cd=34;43:su=30;41:sg=30;46:tw=30;42:ow=30;43'
 export CLICOLOR=true
+export PRIMARY_FG=white
 
 # You may need to manually set your language environment
 export LANG=en_US.UTF-8
@@ -24,6 +25,7 @@ setopt SHARE_HISTORY # share history between sessions ???
 setopt EXTENDED_HISTORY # add timestamps to history
 setopt COMPLETE_IN_WORD
 setopt IGNORE_EOF
+setopt PROMPTSUBST
 
 setopt APPEND_HISTORY # adds history
 setopt HIST_IGNORE_ALL_DUPS  # don't record dupes in history
