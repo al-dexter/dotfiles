864daa8e21a712c92b05bc370b39623a61001ca0
Alexander Bershadskyy <a.bershadskyy@ryte.com>
2018-07-20 14:39:36 +0200

    add allias for quick removing local stale branches

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,2 +1,3 @@
-alias c='clear'          # Clear terminal display
-alias reload='exec zsh'  # Reload the shell
+alias c="clear"          # Clear terminal display
+alias reload="exec zsh"  # Reload the shell
+alias grmp='git branch -vv | grep "origin/.*: gone]" | cut -d" " -f3 | xargs git branch -D'
