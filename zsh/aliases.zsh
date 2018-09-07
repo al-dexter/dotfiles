03febcff9e9f5fb3c961c681818f527895ddd6fd
Alexander Bershadskyy <a.bershadskyy@ryte.com>
2018-09-07 14:52:54 +0200

    change alias for removing stale local branches

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,3 +1,3 @@
 alias c="clear"          # Clear terminal display
 alias reload="exec zsh"  # Reload the shell
-alias grmp='git branch -vv | grep "origin/.*: gone]" | cut -d" " -f3 | xargs git branch -D'
+alias gbrg='git branch -vv | grep "origin/.*: gone]" | cut -d" " -f3 | xargs git branch -D'
