cedec4f82eabf31b16f75a82d7a9c4cbd3b08b71
Alexander Bershadskyy <a.bershadskyy@ryte.com>
2018-10-17 10:52:42 +0200

    once you go black...

--- zsh/config.zsh
+++ zsh/config.zsh
@@ -1,7 +1,7 @@
 export LSCOLORS="exfxcxdxbxegedabagacad"
 export LS_COLORS='di=34:ln=35:so=32:pi=33:ex=31:bd=34;46:cd=34;43:su=30;41:sg=30;46:tw=30;42:ow=30;43'
 export CLICOLOR=true
-export PRIMARY_FG=white
+export PRIMARY_FG=black
 
 # You may need to manually set your language environment
 export LANG=en_US.UTF-8
