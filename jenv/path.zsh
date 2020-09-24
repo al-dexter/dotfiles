8a50e2b8c770dc0d591b5f8957cca242b9616b34
Alexander Bershadsky <a.bershadskyy@ryte.com>
2020-09-24 19:44:00 +0200

    update configs

--- jenv/path.zsh
+++
@@ -1,2 +0,0 @@
-export PATH="$HOME/.jenv/bin:$HOME/.jenv/shims:$PATH"
-eval "$(jenv init - zsh)"
---
+++ sdkman/path.zsh
@@ -0,0 +1,2 @@
+export JAVA_HOME=$HOME/.sdkman/candidates/java/current
+export PATH=$JAVA_HOME/bin:$PATH
